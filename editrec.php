d301ad511c64fe6e343548186a8a070010eaf7cd
Kent <127605021+KentBrian054@users.noreply.github.com>
2023-05-16 15:05:20 +0800

    update

--- editrec.php
+++ editrec.php
@@ -2,98 +2,28 @@
 <html lang="en">
 <head>
       <title>Basura Location</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-  width: 100%;
-  border: 1px solid #ddd;
-}
-
-th, td {
-  text-align: left;
-  padding: 8px;
-}
-
-tr:nth-child(even){background-color: #f2f2f2}
-
-      .button {
-        background-color: #1c87c9;
-        border: none;
-        color: white;
-        padding: 5px 10px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 14px;
-        margin: 1px 1px;
-        cursor: pointer;
-      }
-
-input[type=text], input[type=password] {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  box-sizing: border-box;
-}
-/* Style the submit button */
-input[type=submit] {
-  background-color: #04AA6D;
-  color: white;
-  padding: 12px 20px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  float: left;
-}
-</style>
-
-
-
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="css/style.css">
    </head>   
 
    <body>
-      <center>
          <h1>Updating Data in Database</h1>
-         <table>
          <form action="saveupdaterec.php" method="post">
-
-         <tr><td>
-               <label for="Userid">Id:</label></td><td>
-               <input type="text" name="pid" id="pid" value=<?php echo $_REQUEST["bid"];  ?> >
-               </td></tr>      
-             
-            <tr><td>
-               <label for="dname">Name:</label></td><td>
-               <input type="text" name="pname" id="pname" value='<?php  echo $_REQUEST["name"];  ?>'>
-               </td></tr>
-
-        
-             
-            <tr><td>
-               <label for="email">Email Address:</label></td><td>
-               <input type="text" name="price" id="price" value=<?php echo $_REQUEST["email"];  ?>>
-               </td></tr>
-
-
-               <tr><td>
-               <label for="addres">Address:</label></td><td>
-               <input type="text" name="desc" id="desc" value=<?php echo $_REQUEST["address"];  ?>>
-               </td></tr>
-               
+               <label for="Userid">Product ID:</label></td><td>
+               <input type="text" name="pid" id="pid" placeholder="Product ID" value=<?php echo $_REQUEST["bid"];  ?> >
+               <label for="dname">Product Name:</label></td><td>
+               <input type="text" name="pname" id="pname" placeholder="Product Name" value='<?php  echo $_REQUEST["name"];  ?>'>
+               <label for="email">Product Price:</label></td><td>
+               <input type="text" name="price" id="price" placeholder="Product Price" value=<?php echo $_REQUEST["email"];  ?>>
+               <label for="addres">Product Description:</label></td><td>
+               <input type="text" name="desc" id="desc" placeholder="Product Description" value=<?php echo $_REQUEST["address"];  ?>>
             <input type="submit" value="Submit">
-            </td></tr>
          </form>
  <?php
 
 
 
 ?>
-</table>
-      </center>
    </body>
 </html>
 
--- index.php
+++ index.php
@@ -1,90 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
    <head>
-      <title>Basura Location</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-  width: 100%;
-  border: 2px solid #ddd;
-}
-
-th, td {
-  text-align: left;
-  padding: 8px;
-}
-
-tr:nth-child(even){background-color: #f2f2f2}
-
-      .button {
-        background-color: #1c87c9;
-        border: none;
-        color: white;
-        padding: 5px 10px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 14px;
-        margin: 1px 1px;
-        cursor: pointer;
-      }
-
-input[type=text], input[type=password] {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  box-sizing: border-box;
-}
-/* Style the submit button */
-input[type=submit] {
-  background-color: #04AA6D;
-  color: white;
-  padding: 12px 20px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  float: left;
-}
-</style>
-
-
-
+      <title>Scarlets Pet Shop</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="css/style.css">
    </head>
    <body>
-      <center>
-
-         <h1>Nilda Sari-sari Store</h1>
-         <table>
-         <form action="insertrec.php" method="post">
-
-<tr><td>
-               <label for="Prodid">Product Id:</label></td><td>
-               <input type="text" name="pid" id="pid" required></td></tr>
-                   
-<tr><td>
-               <label for="pname">Product Name:</label></td><td>
-               <input type="text" name="pname" id=pname" required>
-               </td>
-               <tr><td>
-               <label for="price">Product Price:</label></td><td>
-               <input type="text" name="price" id="price" required>
-               </td></tr>
-<tr><td>
-               <label for="desc">Product Description:</label></td><td>
-               <input type="text" name="desc" id="desc" required>
-               </td></tr>
-             
-
-<tr><td>
-            <input type="submit" value="Add Record">
-            </td> <td>
-              <input type="text" id="Search" onkeyup="(myFunction())" placeholder="Search for names.." title="Type in a name">
-               </td></tr>
-         </form>
+    <h1>Scarlets Pet Shop Products</h1>
+    <table>
+      <form action="insertrec.php" method="post">
+          <input type="text" name="pid" id="pid" placeholder="Product ID" required>
+          <input type="text" name="pname" id="pname" placeholder="Product Name" required>
+          <input type="text" name="price" id="price" placeholder="Product Price" required>
+          <input type="text" name="desc" id="desc" placeholder="Product Description" required>
+          <input type="submit" value="Add Record">
+          <input type="text" id="Search" onkeyup="(myFunction())" placeholder="Search for names.." title="Type in a name">
+          </form>
+        
+         
  <?php
  // servername => localhost
  // username => root
@@ -103,9 +36,22 @@ input[type=submit] {
  
  if ($result->num_rows > 0) {
    // output data of each row
-   echo "<table id='myTable' border=2><tr><td>Id</td><td>Name</td><td>Address</td><td>Email</td><td>Action</td></tr>";
+   echo "<table id='myTable' border=2>
+   <tr>
+   <td>Product Id</td>
+   <td>Product Name</td>
+   <td>Product Price</td>
+   <td>Product Description</td>
+   <td>Action</td>
+   </tr>";
    while($row = $result->fetch_assoc()) {
-     echo "<tr><td>" . $row["prod_id"]. "</td><td>" . $row["prod_name"]. "</td><td> " . $row["prod_price"].  "</td><td>" . $row["prod_desc"] .   "</td><td><a href=deleterec.php?bid=".$row["prod_id"]." class='button'>Delete </a>  <a href='editrec.php?bid=".$row["prod_id"]."&name=". $row["prod_name"]."&address=".$row["prod_price"]."&email=".$row["prod_desc"]. "' class='button'>Edit </a>    </td></tr>";
+     echo "<tr><td>" . $row["prod_id"]. "</td>
+     <td>" . $row["prod_name"]. "</td>
+     <td> " . $row["prod_price"].  "</td>
+     <td>" . $row["prod_desc"] .   "</td>
+     <td><a href=deleterec.php?bid=".$row["prod_id"]." class='delete'>Delete </a>
+       <a href='editrec.php?bid=".$row["prod_id"]."&name=". $row["prod_name"]."&address=".$row["prod_price"]."&email=".$row["prod_desc"]. "' class='button'>Edit </a>    </td>
+     </tr>";
    
     }
  } else {
@@ -116,32 +62,7 @@ input[type=submit] {
 
 ?>
 </table>
-      </center>
-
-<script>
-function myFunction() {
-  var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("Search");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[1];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }       
-  }
-}
-</script>
-
-
-
-
    </body>
+   <script src="js/script.js"></script>
 </html>
 
