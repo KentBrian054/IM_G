d241b294b93a43d49ec6270d4ce8b0ed2a6c44d6
CBFrown <baalclark12@gmail.com>
2023-06-15 20:46:05 +0800

    added orders

---
+++ orders.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <form action="addorder.php" method="POST">
+        <input type="text" id="ProdID" name="ProdID" placeholder="Product ID"><br>
+        <input type="text" id="TranID" name="TranID" placeholder="Transaction ID"><br>
+        <input type="text" id="ItemQTY" name="ItemQTY" placeholder="Item Quantity"><br>
+        <input type="submit" value="Save">
+    </form>
+    
+</body>
+<script src="js/script.js"></script>
+</html>
