d921c969c600a320b5157c044dd562240f1cbc66
CBFrown <baalclark12@gmail.com>
2023-06-15 20:01:37 +0800

    Create category.php

---
+++ category.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <form action="save.php" method="POST">
+        <input type="text" id="CategoryID" name="CategoryID" placeholder="Product Category ID"><br>
+        <input type="text" id="CategoryDesc" name="CategoryDesc" placeholder="Product Category Description"><br>
+        <input type="submit" value="Save">
+    </form>
+    
+</body>
+<script src="js/script.js"></script>
+</html>
